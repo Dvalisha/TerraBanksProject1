bae032b6e32a064bd1206a90bb5fb73f52d640f8
Nika <Nika@DESKTOP-GJJISPV>
2023-02-14 21:45:30 +0400

    განახლება

--- TerraBanksProject1/Program.cs
+++ TerraBanksProject1/Program.cs
@@ -3,5 +3,9 @@
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to C#");
+        Console.WriteLine("User");
+        Console.WriteLine("");
+        Console.WriteLine("My name is");
+        Console.WriteLine("Nikusha");
     }
 }   
