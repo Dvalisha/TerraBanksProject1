cdedc934b19bb5b81163185d421c981f68db8e44
Nika <Nika@DESKTOP-GJJISPV>
2023-02-19 14:29:36 +0400

    Changes to Master

--- TerraBanksProject1/Program.cs
+++ TerraBanksProject1/Program.cs
@@ -1,5 +1,9 @@
 ﻿class MyMain
 {
+    /// <summary>
+    /// ტექსტის სტრიქონებად გამოტანა
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to C#");
