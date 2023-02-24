c5971162100c3f04c7741d3f483647c2a6f90852
Nika <Nika@DESKTOP-GJJISPV>
2023-02-24 15:55:22 +0400

    Update

--- TerraBanksProject1/Program.cs
+++ TerraBanksProject1/Program.cs
@@ -1,50 +1,110 @@
-﻿class MyMain
+﻿using System.Runtime.CompilerServices;
+
+class MyMain
 {
     static void Main(string[] args)
     {
         // Homework 3
-        // 1. შევქმანთ სამი ცვლადი (x, z, y). მივანიჭოთ მნიშვნელობები. გამოვიყავნოთ კონსოლში: x + z * y = ჯამი; მაგ. 4, 5, 10-ის შედეგია 54.
-        int x = 4;
-        int y = 10;
-        int z = 5;
-        Console.WriteLine(x + y * z);
+        // 1. პირველი მეთოდის გამოძახება
+        getSum(5, 4);
+        Console.WriteLine();
 
-        // 2. პროგრამა სთავაზობს მომხმარებელს შეიყვანოს სახელი (Archil), გვარი (Sikharulidze), ასაკი (34), სიმართლე (171) და წონა (88.7).
-        // შევინახოთ ეს მონაცემები შესაბამისი ტიპის ცვლადებში. გამოვიყვანოთ მთელი ინფორმაცია კონსოლში.
-        Console.WriteLine("");
-        Console.Write("Please, enter your name: ");
-        string name = Console.ReadLine();
-        Console.Write("Please, enter your surname: ");
-        string surname = Console.ReadLine();
-        Console.Write("Please, enter your age: ");
-        int age = Int32.Parse(Console.ReadLine());
-        Console.Write("Please, enter your height in cm: ");
-        float height = float.Parse(Console.ReadLine());
-        Console.Write("Please, enter your weight in kg: ");
-        float weight = float.Parse(Console.ReadLine());
-        Console.WriteLine("");
-        Console.WriteLine("Your name is: " + name);
-        Console.WriteLine("Your surname is: " + surname);
-        Console.WriteLine("Your age is: " + age);
-        Console.WriteLine("Your height is: " + height);
-        Console.WriteLine("Your weight is: " + weight);
+        //   2. მომხმარებელს შეყავს მონაცემები
+        Console.Write("Please enter Number 1: ");
+        double x1 = double.Parse(Console.ReadLine());
 
-        // 3. დაწერეთ კოდი, რომელიც ითვლის ადამიანის BMI (Body Mass Index) მოიძიეთ ფორმულა ინტერნეტში. ლოგიკა და კოდი დამოუკიდებლად გაწერეთ.
-        float heightForBmi = height / 100;
-        Console.WriteLine("");
-        Console.WriteLine("Your budy index is: " + weight / (heightForBmi * heightForBmi));
+        Console.Write("Please enter Number 2: ");
+        double x2 = double.Parse(Console.ReadLine());
 
-        // 4. დამიხატეთ კონსოლში შემდეგი ნახატი:
-        int num1 = 1;
-        Console.WriteLine("");
-        Console.WriteLine("   |   |   ");
-        Console.WriteLine(" " + num1 + " | " + ++num1 + " | " + ++num1 + " ");
-        Console.WriteLine("___|___|___");
-        Console.WriteLine("   |   |   ");
-        Console.WriteLine(" " + ++num1 + " | " + ++num1 + " | " + ++num1 + " ");
-        Console.WriteLine("___|___|___");
-        Console.WriteLine("   |   |   ");
-        Console.WriteLine(" " + ++num1 + " | " + ++num1 + " | " + ++num1 + " ");
-        Console.WriteLine("   |   |   ");
+        Console.Write("Please enter Number 3: ");
+        double x3 = double.Parse(Console.ReadLine());
+
+        Console.Write("Please enter Number 4: ");
+        double x4 = double.Parse(Console.ReadLine());
+
+        // 2. მეორე მეთოდის გამოძახება
+        Console.WriteLine();
+        minMax(x1, x2, x3, x4);
+        Console.WriteLine();
+
+        //   3. მესამე მეთოდის გამოძახება
+        fruits();
+
+
+        // 1. შევქმნათ მეთოდი, რომელსაც გადავცემთ ორ სრულ რიცხვს. მეთოდმა უნდა დააბრუნოს ორი რიცხვის ჯამი. გამოვიყვანოთ ეს ჯამი შესვლის წერტილის მეთოდის მეშვეობით კონსოლში
+        static void getSum(int x, int y)
+        {
+        Console.WriteLine(x + y);
+        }
+
+        //   2. შევქმნათ მეთოდი. ვთხოვოთ მომხმარებელს შეიყვანოს ოთხი ნებისმიეირ ტიპის რიცხვი (ანუ არა მხოლოდ ინტეჯერი).
+        //   მეთოდის ფარგლებში უნდა მოიძებნოს ამ ოთხი რიცხვიდან ყველაზე დიდი და ყველაზე პატარა. მეთოდმა უნდა გამოიყვანოს კონსოლში ეს ორი რიცხვი.
+        static void minMax(double x1,double x2, double x3, double x4)
+        {
+            if (x1 >= x2 && x1 >= x3 && x1 >= x4) 
+            {
+                Console.WriteLine("The highest number is: " + x1);
+            }
+            else if (x2 >= x1 && x2 >= x3 && x2 >= x4) 
+            {
+                Console.WriteLine("The highest number is: " + x2);
+            }
+            else if (x3 >= x1 && x3 >= x2 && x3 >= x4)
+            {
+                Console.WriteLine("The highest number is: " + x3);
+            }
+            else 
+            {
+                Console.WriteLine("The highest number is: " + x4);
+            }
+
+            if (x1 <= x2 && x1 <= x3 && x1 <= x4)
+            {
+                Console.WriteLine("The lowest number is: " + x1);
+            }
+            else if (x2 <= x1 && x2 <= x3 && x2 <= x4)
+            {
+                Console.WriteLine("The lowest number is: " + x2);
+            }
+            else if (x3 <= x1 && x3 <= x2 && x3 <= x4)
+            {
+                Console.WriteLine("The lowest number is: " + x3);
+            }
+            else 
+            {
+                Console.WriteLine("The lowest number is: " + x4);
+            }
+        }
+
+        //   3. შევქმნათ მეთოდი, რომელმაც უნდა მიიღოს მეორე მეთოდის მეშვეობით მომხმარებლისგან ხილის დასახელება.
+        //   ამ ხილის დასახელების მიღების შემდეგ switch ოპერატორის მეშვეობით ნახოს თუ რომელი ხილია და გამოიყვანოს შესაბამისი შეტყობინება.
+        //   თუ ასეთი ხილი არ მოიძებნა, ამის შესახებაც მომხმარებელმა უნდა მიიღოს შეტყობინება. ხილი: apple, watermelon, melon, cherry, strawberry.
+        static void fruits()
+        {
+            Console.Write("Please enter a fruit: ");
+            string fruitName = Console.ReadLine();
+            switch (fruitName) 
+            {
+                case "apple":
+                    Console.WriteLine("Fruit: apple");
+                    break;
+                case "watermelon":
+                    Console.WriteLine("Fruit: watermelon");
+                    break;
+                case "melon":
+                    Console.WriteLine("Fruit: melon");
+                    break;
+                case "cherry":
+                    Console.WriteLine("Fruit: cherry");
+                    break;
+                case "strawberry":
+                    Console.WriteLine("Fruit: strawberry");
+                    break;
+                default:
+                    Console.WriteLine("There is no such fruit");
+                    break;
+            Console.WriteLine();
+            }
+        }
     }
 }   
