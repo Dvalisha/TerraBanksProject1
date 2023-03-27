eaa201bb553217c61626098d4e7defa38cef84d8
Nika <Nika@DESKTOP-GJJISPV>
2023-03-27 17:05:16 +0400

    Update

--- TerraBanksProject1/Program.cs
+++ TerraBanksProject1/Program.cs
@@ -6,128 +6,140 @@ class MyMain
 {
     static void Main(string[] args)
     {
-        //1. დაწერეთ პროგრამა, რომელიც გამოთვლის კონსოლიდან შეყვანილი ორი რიცხვის ნამრავლს.
+        //1. if/else ბლოკების გამოყენებით დაწერეთ ფუნქცია, რომელიც გამოიტანს გადაცემული რიცხვი კენტია თუ ლუწი.
+        Console.Write("Please enter a number: ");
+        int a = Int32.Parse(Console.ReadLine());
 
-        Console.Write("Please, enter first number: ");
-        int x1 = Int32.Parse(Console.ReadLine());
-
-        Console.Write("Please, enter second number: ");
-        int x2 = Int32.Parse(Console.ReadLine());
-
-        int product = x1 * x2;
-
-        Console.Write(x1 + x2);
-        Console.WriteLine();
-
-        //2. დაწერეთ პროგრამა, რომელიც კონსოლზე გამოიტანს თქვენ სახელს, გვარს და ასაკს.
-
-        Console.WriteLine();
-        Console.Write("Please, enter your name: ");
-        string name = Console.ReadLine();
-
-        Console.Write("Please, enter your surname: ");
-        string surname = Console.ReadLine();
-
-        Console.Write("Please, enter your age: ");
-        int age = Int32.Parse(Console.ReadLine());
-
-        Console.Write("Your name is " + name + ".Your surname is " + surname + ".Your age is " + age + ".");
-        Console.WriteLine();
-
-        //3. დაწერეთ პროგრამა, რომელიც გამოიტანს ნაშთს კონსოლიდან გადაცემული ორი რიცხვის გაყოფის შედეგად.
-
-        Console.WriteLine();
-        Console.Write("Please, enter dividen: ");
-        int dividend = Int32.Parse(Console.ReadLine());
-
-        Console.Write("Please, enter divisor: ");
-        int divisor = Int32.Parse(Console.ReadLine());
-
-        int remainder = dividend % divisor;
-
-        Console.Write("The remainder is: " + remainder);
-        Console.WriteLine();
-
-        //4. შედარების ლოგიკური ოპერატორების საშუალებით შეადარეთ რიცხვები და დაწერეთ პროგრამა, რომელიც გამოიტანს true ან false.
-
-        Console.WriteLine();
-        Console.Write("Please, enter first number: ");
-        int y1 = Convert.ToInt32(Console.ReadLine());
-
-        Console.Write("Please, enter second number: ");
-        int y2 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine();
-
-        if (y1 >= y2)
+        if (a % 2 == 0) 
         {
-            Console.WriteLine("True");
-        } 
+            Console.WriteLine("Your number is even");
+        }
         else
         {
-            Console.WriteLine("False");
+            Console.WriteLine("Your number is odd");
         }
+
         Console.WriteLine();
 
-        //5. ჩაწერეთ შემდეგი პირობა Boolean ლოგიკური ოპერატორის გამოყენებით: A>B და A>C, A>B ან A>C. როცა A=5 B=4 C=7
+        //2. Switch/Case ბლოკის გამოყენებით დაწერეთ ფუნქცია, რომელიც გადაცემული რიცხვის მიხედვით გამოითვლის რომელი დღეა დღეს. მაგ. 1 ნიშნავს ორშაბათს, 2 სამშაბათს და ა.შ.
 
-        int a = 5;
-        int b = 4;
-        int c = 7;
+        Console.Write("Please enter a number for day (1-7): ");
+        int b = Int32.Parse(Console.ReadLine());
 
-        bool logic1 = a > b && a > c;
-        Console.WriteLine(logic1);
-
-        bool logic2 = a > b || a > c;
-        Console.WriteLine(logic2);
-        Console.WriteLine();
-
-        //6. შექმენით რიცხვითი ტიპის, 10 ელემენტისაგან შემდგარი მასივი. კონსოლზე გამოიტანეთ პირველი და მეშვიდე ელემენტი.
-        
-        int[] numbers = new int[10] { 20, -5, 39, 47, -99, 165, 300, 0, 68, -5 };
-
-        Console.WriteLine("First element is: " + numbers[0]);
-        Console.WriteLine("Seventh element is: " + numbers[6]);
-        Console.WriteLine();
-
-        //7. შექმენით მასივები შემდეგი განზომილებებით და შეავსე რიცხვებით: ორგანზომილებიანი [3,2], [4,2]. სამგზანზომილებიანი [2,2,2], [1,2,3]
-
-        int[,] array2A = new int[3, 2]
+        switch (b)
         {
-            {13, 25 },
-            {-5, -13 },
-            {47, -12 }
-        };
+            case 1: Console.WriteLine("Today is Monday");
+                break;
+            case 2: Console.WriteLine("Today is Tuesday");
+                break;
+            case 3: Console.WriteLine("Today is Wednesday");
+                break;
+            case 4: Console.WriteLine("Today is Thursday");
+                break;
+            case 5: Console.WriteLine("Today is Friday");
+                break;
+            case 6: Console.WriteLine("Today is Saturday");
+                break;
+            case 7: Console.WriteLine("Today is Sunday");
+                break;
+        }
 
-        Console.Write(array2A[0, 1]);
-        Console.WriteLine();
+        //2.1 ნებისმიერი რიცხვზე რომ თქვას დღე:
 
-        int[,] array2B = new int[4, 2]
+        switch (b % 7)
         {
-            {3, 2 },
-            {12, 885 },
-            {27, -1 },
-            {313, 0 }
-        };
+            case 1:
+                Console.WriteLine("Today is Monday");
+                break;
+            case 2:
+                Console.WriteLine("Today is Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Today is Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Today is Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Today is Friday");
+                break;
+            case 6:
+                Console.WriteLine("Today is Saturday");
+                break;
+            case 0:
+                Console.WriteLine("Today is Sunday");
+                break;
+        }
 
-        Console.Write(array2B[2, 0]);
         Console.WriteLine();
 
-        int[,,] array3A = new int[2, 2, 2]
+        //3. შექმენით რიცხვიდი ცვლადი. While ციკლის საშუალებით გამოიტანეთ კონსოლზე რიცხვი იქამდე სანამ ის არ მიიღებს უარყოფით მნიშვნელობას.
+
+        int c = 10;
+
+        while (c >= 0)
         {
-            { {77, 87 }, {2, 18 } },
-            { {0, 475 }, {36, -15 } }
-        };
+            Console.WriteLine("Number = {0}", c);
+            c--;
+        }
 
-        Console.Write(array3A[1, 1, 0]);
         Console.WriteLine();
 
-        int[,,] array3B = new int[1, 2, 3]
+        //4. Do/While ციკლის საშუალებით გამოიტანეთ კონსოლიდან შეყვანილი და შესაბამისი რიცხვები, სანამ ჩვენ მიერ შეტანილი რიცხვი არ მიიღებს უარყოფით მნიშვნელობას.
+
+        Console.Write("Please enter a number: ");
+        int d = Int32.Parse(Console.ReadLine());
+
+        do
         {
-            { {17, -61, -11 }, {256, 11, 51 } },
-        };
+            Console.WriteLine("Number = {0}", d);
+            d--;
+        } while(d >= 0);
 
-        Console.Write(array3B[0, 0, 2]);
         Console.WriteLine();
 
+        //5. Do/While ციკლის საშუალებით გამოიტანეთ თქვენი დაბადების წელი და გაზარდეთ 2015 წლამდე. 2015 წლის შემდეგ მონაცემები აღარ უნდა დაიბეჭდოს.
+
+        int e = 1990;
+
+        do
+        {
+            Console.WriteLine("Year = {0}", e);
+            e++;
+        } while(e < 2016);
+
+        Console.WriteLine();
+
+        //6. გამოიტანეთ 1-დან 10-მდე ნატურალური რიცხვები while ციკლის საშუალებით.
+
+        int f = 1;
+
+        while (f <10)
+        {
+            Console.WriteLine("Number = {0}", f);
+            f++;
+        }
+
+        Console.WriteLine();
+
+        //7. For ციკლის საშუალებით გამოთვალეთ და კონსოლზე გამოიტანეთ ყველა ლუწი რიცხვი, იმ ორი რიცხვის დიაპაზონში რომელსაც გადავცემთ კონსოლიდან.
+
+        Console.Write("Please enter first number: ");
+        int g = Int32.Parse(Console.ReadLine());
+
+        Console.Write("Please enter second number: ");
+        int h = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < (h - g) / 2 ; i++)
+        {
+            if(g % 2 == 0)
+            {
+                Console.WriteLine("Number = {0}", g + (i * 2));
+            } 
+            else
+            {
+                Console.WriteLine("Number = {0}", g + 1 + (i * 2));
+            }
+        }
     }
 }
